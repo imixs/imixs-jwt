e8f8d494e4120bab22a3a7ba0f04538c28424520
Ralph Soika <ralph.soika@imixs.com>
2020-01-08 21:40:03 +0100

    fixed
    Issue #14

--- src/main/java/org/imixs/jwt/HMAC.java
+++ src/main/java/org/imixs/jwt/HMAC.java
@@ -48,62 +48,60 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class HMAC {
 
-	/**
-	 * Computes a Hash-based Message Authentication Code (HMAC) for the
-	 * specified secret key and message.
-	 *
-	 * @param secretKey
-	 *            The secret key, with the appropriate HMAC algorithm. Must not
-	 *            be {@code null}.
-	 * @param message
-	 *            The message. Must not be {@code null}.
-	 * @return A MAC service instance.
-	 */
-	public static byte[] createSignature(final SecretKey secretKey, final byte[] message)
-			throws InvalidKeyException, NoSuchAlgorithmException {
+    /**
+     * Computes a Hash-based Message Authentication Code (HMAC) for the specified
+     * secret key and message.
+     *
+     * @param secretKey The secret key, with the appropriate HMAC algorithm. Must
+     *                  not be {@code null}.
+     * @param message   The message. Must not be {@code null}.
+     * @return A MAC service instance.
+     */
+    public static byte[] createSignature(final SecretKey secretKey, final byte[] message)
+            throws InvalidKeyException, NoSuchAlgorithmException {
 
-		Mac mac;
-		mac = Mac.getInstance(secretKey.getAlgorithm());
-		mac.init(secretKey);
-		mac.update(message);
-		return mac.doFinal();
-	}
+        Mac mac;
+        mac = Mac.getInstance(secretKey.getAlgorithm());
+        mac.init(secretKey);
+        mac.update(message);
+        return mac.doFinal();
+    }
 
-	/**
-	 * generates a key
-	 * 
-	 * @param alg
-	 * @param secret
-	 * @return
-	 */
-	public static SecretKey createKey(final String alg, final byte[] secret) {
-		return new SecretKeySpec(secret, alg);
-	}
-	
-	
-	/**
-	 * Base64 encoding without padding 
-	 * 
-	 * @param data
-	 * @return encoded data
-	 */
-	public static String encodeBase64(byte[] data) {
-		byte[] encoded = Base64.getEncoder().withoutPadding().encode(data);
+    /**
+     * generates a key
+     * 
+     * @param alg
+     * @param secret
+     * @return
+     */
+    public static SecretKey createKey(final String alg, final byte[] secret) {
+        return new SecretKeySpec(secret, alg);
+    }
 
-		return new String(encoded, StandardCharsets.UTF_8);
+    /**
+     * Base64 encoding without padding
+     * 
+     * @param data
+     * @return encoded data
+     */
+    public static String encodeBase64(byte[] data) {
+        // issue #14 - use Base64.getUrlEncoder() instead of Base64.getEncoder()
+        byte[] encoded = Base64.getUrlEncoder().withoutPadding().encode(data);
 
-	}
-	
-	/**
-	 * Base64 encoding without padding 
-	 * 
-	 * @param data
-	 * @return decoded data
-	 */
-	public static String decodeBase64(byte[] data) {
-		
-		byte[] decoded=Base64.getDecoder().decode(data);
-		return new String(decoded, StandardCharsets.UTF_8);
-	}
+        return new String(encoded, StandardCharsets.UTF_8);
+
+    }
+
+    /**
+     * Base64 encoding without padding
+     * 
+     * @param data
+     * @return decoded data
+     */
+    public static String decodeBase64(byte[] data) {
+        // issue #14 - use Base64.getUrlDecoder() instead of Base64.getDecoder()
+        byte[] decoded = Base64.getUrlDecoder().decode(data);
+        return new String(decoded, StandardCharsets.UTF_8);
+    }
 
 }
--- src/test/java/org/imixs/jwt/JWTBuilderTest.java
+++ src/test/java/org/imixs/jwt/JWTBuilderTest.java
@@ -42,181 +42,175 @@ import junit.framework.Assert;
  */
 public class JWTBuilderTest {
 
-  private static Logger logger = Logger.getLogger(JWTBuilderTest.class.getName());
+    private static Logger logger = Logger.getLogger(JWTBuilderTest.class.getName());
 
-  String header;
-  String payload;
-  String encodedHeader;
-  String payload_without_iat;
-  String encodedPayload;
-  String secret;
-  String algorithm; // "HmacMD5"
-  String signature;
-  String token;
+    String header;
+    String payload;
+    String encodedHeader;
+    String payload_without_iat;
+    String encodedPayload;
+    String secret;
+    String algorithm; // "HmacMD5"
+    String signature;
+    String token;
 
-  /**
-   * Data based on iwt.io debugger example
-   */
-  @Before
-  public void setup() {
-    header = "{\"alg\":\"HS256\",\"typ\":\"JWT\"}";
-    payload = "{\"iat\":1578218678992,\"sub\":\"1234567890\",\"name\":\"John Doe\",\"admin\":true}";
-    payload_without_iat = "{\"sub\":\"1234567890\",\"name\":\"John Doe\",\"admin\":true}";
-    encodedHeader = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
-    encodedPayload = "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
-    secret = "secret";
-    algorithm = "HmacSHA256";
-    signature = "P0tXidx2nGdGhmJ9+0bcNqDbM5zU+WKGib0u5osG7Gc";// expected
-                                                              // base64
-                                                              // encoded
-                                                              // signature
+    /**
+     * Data based on iwt.io debugger example
+     */
+    @Before
+    public void setup() {
+        header = "{\"alg\":\"HS256\",\"typ\":\"JWT\"}";
+        payload = "{\"iat\":1578218678992,\"sub\":\"1234567890\",\"name\":\"John Doe\",\"admin\":true}";
+        payload_without_iat = "{\"sub\":\"1234567890\",\"name\":\"John Doe\",\"admin\":true}";
+        encodedHeader = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+        encodedPayload = "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
+        secret = "secret";
+        algorithm = "HmacSHA256";
+        signature = "P0tXidx2nGdGhmJ9+0bcNqDbM5zU+WKGib0u5osG7Gc";// expected
+                                                                  // base64
+                                                                  // encoded
+                                                                  // signature
 
-    // token =
-    // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOiIxMjMiLCJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.P0tXidx2nGdGhmJ9+0bcNqDbM5zU+WKGib0u5osG7Gc";
-    token =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NzgyMTg2Nzg5OTIsInN1YiI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJhZG1pbiI6dHJ1ZX0.ujX9a3mbNIq2+z3/5tam0UujZCSaJQKYe43av2xnIzI";
-  }
-
-  /**
-   * Test signature with encoded data, based on the iwt.io debugger example
-   * 
-   * https://jwt.io/
-   * 
-   * @throws JWTException
-   * 
-   * @throws InvalidKeyException
-   * @throws SignatureException
-   * @throws NoSuchAlgorithmException
-   * @throws IllegalStateException
-   * @throws UnsupportedEncodingException
-   */
-  @Test
-  public void testSignatureEncoded() throws JWTException {
-
-    SecretKey secretKey = HMAC.createKey(algorithm, secret.getBytes());
-
-    JWTBuilder builder = new JWTBuilder().setKey(secretKey).setEncodedHeader(encodedHeader)
-        .setEncodedPayload(encodedPayload).sign();
-
-    logger.info("header=" + HMAC.decodeBase64(encodedHeader.getBytes()));
-    logger.info("payload=" + HMAC.decodeBase64(encodedPayload.getBytes()));
-    logger.info("signature=" + builder.getSignature());
-    Assert.assertEquals("TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ", builder.getSignature());
-
-  }
-
-  /**
-   * Test signature with JSON data, based on the iwt.io debugger example
-   * 
-   * https://jwt.io/
-   * 
-   * @throws JWTException
-   * 
-   * @throws InvalidKeyException
-   * @throws SignatureException
-   * @throws NoSuchAlgorithmException
-   * @throws IllegalStateException
-   * @throws UnsupportedEncodingException
-   */
-  @Test
-  public void testSignatureJSON() throws JWTException {
-
-    SecretKey secretKey = HMAC.createKey(algorithm, secret.getBytes());
-
-    JWTBuilder builder =
-        new JWTBuilder().setKey(secretKey).setHeader(header).setPayload(payload).sign();
-
-    logger.info("signature=" + builder.getSignature());
-    Assert.assertEquals("ujX9a3mbNIq2+z3/5tam0UujZCSaJQKYe43av2xnIzI", builder.getSignature());
-
-
-  }
-
-  /**
-   * Test JSON Web token, based on the iwt.io debugger example
-   * 
-   * https://jwt.io/
-   * 
-   * @throws JWTException
-   * 
-   * @throws InvalidKeyException
-   * @throws SignatureException
-   * @throws NoSuchAlgorithmException
-   * @throws IllegalStateException
-   * @throws UnsupportedEncodingException
-   */
-  @Test
-  public void testJSONWebToken() throws JWTException {
-
-    SecretKey secretKey = HMAC.createKey(algorithm, secret.getBytes());
-
-    JWTBuilder builder =
-        new JWTBuilder().setKey(secretKey).setHeader(header).setPayload(payload).sign();
-
-    logger.info("JWT=" + builder.getToken());
-    Assert.assertEquals(token, builder.getToken());
-
-
-    // test short form
-    builder = new JWTBuilder().setKey(secretKey).setPayload(payload);
-
-    logger.info("JWT=" + builder.getToken());
-    Assert.assertEquals(token, builder.getToken());
-
-
-  }
-
-
-
-  /**
-   * Test JSON Web token, with missing iat. Expectation : iat is created
-   * 
-   * https://jwt.io/
-   * 
-   * @throws JWTException
-   * 
-   * @throws InvalidKeyException
-   * @throws SignatureException
-   * @throws NoSuchAlgorithmException
-   * @throws IllegalStateException
-   * @throws UnsupportedEncodingException
-   */
-  @Test
-  public void testJSONWebTokenWithIat() throws JWTException {
-
-    SecretKey secretKey = HMAC.createKey(algorithm, secret.getBytes());
-
-    JWTBuilder builder =
-        new JWTBuilder().setKey(secretKey).setHeader(header).setPayload(payload_without_iat).sign();
-
-    String iat_token = builder.getToken();
-    logger.info("JWT=" + iat_token);
-
-    // recalculate the payload and test if 'iat' is available...
-
-    String iat_payload =
-        new JWTParser().setKey(secretKey).setToken(iat_token).verify().getPayload();
-
-
-    logger.info("Payload=" + iat_payload);
-
-    // now parse the json .....
-    JsonReader reader = Json.createReader(new StringReader(iat_payload));
-    JsonObject payloadObject = reader.readObject();
-
-
-    try {
-      // get the iat.....
-      long iat = payloadObject.getInt("iat");
-      Date now = new Date();
-      logger.info("iat=" + iat + " -> now=" + now.getTime());
-      Assert.assertTrue(iat > 0 && iat <= now.getTime());
-    } catch (NullPointerException e) {
-      Assert.fail();
+        // token =
+        // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOiIxMjMiLCJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.P0tXidx2nGdGhmJ9+0bcNqDbM5zU+WKGib0u5osG7Gc";
+        //    token =
+        // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NzgyMTg2Nzg5OTIsInN1YiI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJhZG1pbiI6dHJ1ZX0.ujX9a3mbNIq2+z3/5tam0UujZCSaJQKYe43av2xnIzI";
+        token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NzgyMTg2Nzg5OTIsInN1YiI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJhZG1pbiI6dHJ1ZX0.ujX9a3mbNIq2-z3_5tam0UujZCSaJQKYe43av2xnIzI";
     }
 
+    /**
+     * Test signature with encoded data, based on the iwt.io debugger example
+     * 
+     * https://jwt.io/
+     * 
+     * @throws JWTException
+     * 
+     * @throws InvalidKeyException
+     * @throws SignatureException
+     * @throws NoSuchAlgorithmException
+     * @throws IllegalStateException
+     * @throws UnsupportedEncodingException
+     */
+    @Test
+    public void testSignatureEncoded() throws JWTException {
 
+        SecretKey secretKey = HMAC.createKey(algorithm, secret.getBytes());
 
-  }
+        JWTBuilder builder = new JWTBuilder().setKey(secretKey).setEncodedHeader(encodedHeader)
+                .setEncodedPayload(encodedPayload).sign();
+
+        logger.info("header=" + HMAC.decodeBase64(encodedHeader.getBytes()));
+        logger.info("payload=" + HMAC.decodeBase64(encodedPayload.getBytes()));
+        logger.info("signature=" + builder.getSignature());
+
+        logger.info("token=" + builder.getToken());
+
+        Assert.assertEquals("TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ", builder.getSignature());
+
+    }
+
+    /**
+     * Test signature with JSON data, based on the iwt.io debugger example
+     * 
+     * https://jwt.io/
+     * 
+     * @throws JWTException
+     * 
+     * @throws InvalidKeyException
+     * @throws SignatureException
+     * @throws NoSuchAlgorithmException
+     * @throws IllegalStateException
+     * @throws UnsupportedEncodingException
+     */
+    @Test
+    public void testSignatureJSON() throws JWTException {
+
+        SecretKey secretKey = HMAC.createKey(algorithm, secret.getBytes());
+
+        JWTBuilder builder = new JWTBuilder().setKey(secretKey).setHeader(header).setPayload(payload).sign();
+
+        logger.info("signature=" + builder.getSignature());
+        logger.info("token=" + builder.getToken());
+
+        Assert.assertEquals("ujX9a3mbNIq2-z3_5tam0UujZCSaJQKYe43av2xnIzI", builder.getSignature());
+
+    }
+
+    /**
+     * Test JSON Web token, based on the iwt.io debugger example
+     * 
+     * https://jwt.io/
+     * 
+     * @throws JWTException
+     * 
+     * @throws InvalidKeyException
+     * @throws SignatureException
+     * @throws NoSuchAlgorithmException
+     * @throws IllegalStateException
+     * @throws UnsupportedEncodingException
+     */
+    @Test
+    public void testJSONWebToken() throws JWTException {
+
+        SecretKey secretKey = HMAC.createKey(algorithm, secret.getBytes());
+
+        JWTBuilder builder = new JWTBuilder().setKey(secretKey).setHeader(header).setPayload(payload).sign();
+
+        logger.info("JWT=" + builder.getToken());
+        Assert.assertEquals(token, builder.getToken());
+
+        // test short form
+        builder = new JWTBuilder().setKey(secretKey).setPayload(payload);
+
+        logger.info("JWT=" + builder.getToken());
+        Assert.assertEquals(token, builder.getToken());
+
+    }
+
+    /**
+     * Test JSON Web token, with missing iat. Expectation : iat is created
+     * 
+     * https://jwt.io/
+     * 
+     * @throws JWTException
+     * 
+     * @throws InvalidKeyException
+     * @throws SignatureException
+     * @throws NoSuchAlgorithmException
+     * @throws IllegalStateException
+     * @throws UnsupportedEncodingException
+     */
+    @Test
+    public void testJSONWebTokenWithIat() throws JWTException {
+
+        SecretKey secretKey = HMAC.createKey(algorithm, secret.getBytes());
+
+        JWTBuilder builder = new JWTBuilder().setKey(secretKey).setHeader(header).setPayload(payload_without_iat)
+                .sign();
+
+        String iat_token = builder.getToken();
+        logger.info("JWT=" + iat_token);
+
+        // recalculate the payload and test if 'iat' is available...
+
+        String iat_payload = new JWTParser().setKey(secretKey).setToken(iat_token).verify().getPayload();
+
+        logger.info("Payload=" + iat_payload);
+
+        // now parse the json .....
+        JsonReader reader = Json.createReader(new StringReader(iat_payload));
+        JsonObject payloadObject = reader.readObject();
+
+        try {
+            // get the iat.....
+            long iat = payloadObject.getInt("iat");
+            Date now = new Date();
+            logger.info("iat=" + iat + " -> now=" + now.getTime());
+            Assert.assertTrue(iat > 0 && iat <= now.getTime());
+        } catch (NullPointerException e) {
+            Assert.fail();
+        }
+
+    }
 
 }
